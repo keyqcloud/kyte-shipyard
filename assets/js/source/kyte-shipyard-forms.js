215b9d8bb3be37eca11cd825e83d968eb3e2ace9
Kenneth P. Hough <kenneth@egtech.us>
2022-09-21 16:17:00 -0500

    Add base forms function to be shared

---
+++ assets/js/source/kyte-shipyard-forms.js
@@ -0,0 +1,10 @@
+
+function createForm(form_selector, title, model, elements, hidden = null, tbl = null, btn_add_selector, modal = true) {
+    var form = new KyteForm(k, $(form_selector), model, hidden, elements, title, tbl, modal, $(btn_add_selector));
+    form.init();
+    if (tbl) {
+        tbl.bindEdit(form);
+    }
+
+    return form;
+}
