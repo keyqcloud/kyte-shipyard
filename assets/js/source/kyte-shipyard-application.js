39ecfff351940d422dffcc166285c859e5baddc5
Kenneth P. Hough <kenneth@egtech.us>
2022-12-29 12:25:00 -0600

    Add application management

---
+++ assets/js/source/kyte-shipyard-application.js
@@ -0,0 +1,30 @@
+let elements = [
+    [
+        {
+            'field':'name',
+            'type':'text',
+            'label':'App Name',
+            'required':true
+        }
+    ]
+];
+
+let colDefApps = [
+    {'targets':0,'data':'name','label':'Name'},
+    {'targets':1,'data':'identifier','label':'Identifier'},
+];
+
+$(document).ready(function() {
+    let navbar = new KyteNav("#mainnav", [], null, 'Kyte Shipyard<sup>&trade;</sup>');
+    navbar.create();
+
+    $('#pageLoaderModal').modal('show');
+    if (k.isSession()) {
+        var dataTable = createTable("#models-table", "Application", colDefApps, null, null, false, true, '/app/detail/', 'id', true);
+        var modalForm = new KyteForm(k, $("#modalForm"), 'Application', null, elements, 'My App', dataTable, true, $("#new"));
+        modalForm.init();
+        dataTable.bindEdit(modalForm);
+    } else {
+        location.href="/?redir="+encodeURIComponent(window.location);
+    }
+});
