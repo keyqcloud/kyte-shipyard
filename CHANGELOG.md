74786980b21ae895344fcefc1d41dbafd7e73048
Kenneth P. Hough <kenneth@egtech.us>
2023-10-05 12:00:29 -0500

    Add footer and version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.1.5
+
+* Add footer to platform
+* Add version number to footer
+
 ## 1.1.4
 
 * Add ability to download page from page editor
---
+++ assets/js/source/kyte-shipyard.js
@@ -0,0 +1,6 @@
+var KS_VERSION = '1.1.5';
+
+$(document).ready(function() {
+    document.getElementById("kyteShipyardVersion").textContent = KS_VERSION;
+    document.getElementById("kyteJSVersion").textContent = typeof Kyte.VERSION !== 'undefined' ? Kyte.VERSION : 'pre-1.0.5';
+});
