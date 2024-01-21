df220ee635053e65812a0f76eda361439e16cb8b
Kenneth Hough <kenneth@keyqcloud.com>
2024-01-21 11:14:25 -0600

    Fix bug with obfuscate script that created a new folder

--- obfuscate.bat
+++ obfuscate.bat
@@ -1,13 +1,5 @@
 @echo off
 
-:obfuscate
-set "filename=%~1"
-set "output=%filename%"
-set "output=%output:source\=%"
-echo Obfuscating: %filename% Output: %output%
-javascript-obfuscator.cmd "%filename%" --output "%output%" --compact true --string-array-encoding base64 --string-array-wrappers-type variable
-goto :eof
-
 :: Update version in kyte-shipyard.js if version argument is provided
 if "%~2" neq "" (
     powershell -Command "(Get-Content assets\js\source\kyte-shipyard.js) -replace '^var KS_VERSION = .*;', 'var KS_VERSION = '''%~2''';' | Set-Content assets\js\source\kyte-shipyard.js"
@@ -19,6 +11,15 @@ if "%~1"=="" (
     for %%f in (assets\js\source\*.js) do (
         call :obfuscate "%%f"
     )
+    :obfuscate
+        set "filename=%1"
+        set "output=%filename%"
+        set "output=%output:source\=%"
+        if not "%filename%"=="" if not "%output%"=="" (
+            echo Obfuscating: %filename% Output: %output%
+            javascript-obfuscator.cmd "%filename%" --output "%output%" --compact true --string-array-encoding base64 --string-array-wrappers-type variable
+        )
+        goto :eof
 ) else (
     :: Check if the path contains 'source' and obfuscate a single file if true
     echo %~1 | findstr /C:"source" > nul
