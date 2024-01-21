a49cae61f035656e7fd735de30790d1a29a1e0ed
Kenneth Hough <kenneth@keyqcloud.com>
2024-01-21 09:43:37 -0600

    Update obfuscation script to work on Windows

---
+++ obfuscate.bat
@@ -0,0 +1,34 @@
+@echo off
+
+:obfuscate
+set "filename=%~1"
+set "output=%filename%"
+set "output=%output:source\=%"
+echo Obfuscating: %filename% Output: %output%
+javascript-obfuscator.cmd "%filename%" --output "%output%" --compact true --string-array-encoding base64 --string-array-wrappers-type variable
+goto :eof
+
+:: Update version in kyte-shipyard.js if version argument is provided
+if "%~2" neq "" (
+    powershell -Command "(Get-Content assets\js\source\kyte-shipyard.js) -replace '^var KS_VERSION = .*;', 'var KS_VERSION = '''%~2''';' | Set-Content assets\js\source\kyte-shipyard.js"
+)
+
+:: Obfuscate all files in the source directory if no specific file is specified
+if "%~1"=="" (
+    echo Obfuscating all JavaScript files...
+    for %%f in (assets\js\source\*.js) do (
+        call :obfuscate "%%f"
+    )
+) else (
+    :: Check if the path contains 'source' and obfuscate a single file if true
+    echo %~1 | findstr /C:"source" > nul
+    if not errorlevel 1 (
+        call :obfuscate "%~1"
+
+        echo Obfuscating KyteShipyard JS file
+        call :obfuscate "assets\js\source\kyte-shipyard.js"
+    ) else (
+        echo Please use absolute file paths or execute from root project path.
+    )
+)
+goto :eof
